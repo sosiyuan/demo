61ab1743155a2b0e212807f9c0fac39613044cb2
SoSiYuan <453694099@qq.com>
2023-03-26 22:06:13 +0800

    静态创建对象是线程安全的；单例模式的非线程安全写法

--- thread_demo/signal_demo.cpp
+++ thread_demo/signal_demo.cpp
@@ -1,16 +1,76 @@
 #include "signal_demo.h"
 #include <iostream>
-extern void SignalDemo();
+#include <thread>
+
+namespace ThreadSafeByStaticConstract
+{
+    // 线程安全 —— 静态创建对象
+    struct StaticClassDemo
+    {
+        StaticClassDemo()
+        {
+            std::cout << "StaticClassDemo: " << this << std::endl;
+        }
+        ~StaticClassDemo()
+        {
+            std::cout << "~StaticClassDemo: " << this << std::endl;
+        }
+        static StaticClassDemo *GetInstance()
+        {
+            // 非线程安全
+            if (p == nullptr)
+            {
+                p = new StaticClassDemo();
+            }
+            return p;
+        }
+
+    private:
+        static StaticClassDemo *p;
+    };
+    StaticClassDemo *StaticClassDemo::p = nullptr;
+
+    sem_t TestStaticSem;
+
+    void TestStatic()
+    {
+#if 0
+        // 静态变量的构造函数、析构函数是线程安全的
+        static StaticClassDemo staticClassDemo;
+        StaticClassDemo *pStaticClassDemo = &staticClassDemo;
+#else
+        StaticClassDemo *pStaticClassDemo = StaticClassDemo::GetInstance();
+#endif
+        sem_wait(&TestStaticSem);
+        std::cout << "TestStatic:" << pStaticClassDemo << std::endl;
+        sem_post(&TestStaticSem);
+    }
+
+    void DemoMain()
+    {
+        sem_init(&TestStaticSem, 0, 0);
+        std::thread t1(TestStatic);
+        std::thread t2(TestStatic);
+        std::thread t3(TestStatic);
+        std::thread t4(TestStatic);
+        std::thread t5(TestStatic);
+
+        sem_post(&TestStaticSem);
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+        t5.join();
+        std::cout << "Hello World" << std::endl;
+        sem_destroy(&TestStaticSem);
+    }
+}; // namespace ThreadSafeByStaticConstract
 
 static struct RegDemoFuncS
 {
     RegDemoFuncS()
     {
-        RegDemoFunc()(SemDemo);
+        RegDemoFunc()(ThreadSafeByStaticConstract::DemoMain);
+        // RegDemoFunc()(SemDemo);
     }
 } s_regDemoFunc;
-
-void SignalDemo()
-{
-    std::cout << "Hello World" << std::endl;
-}
